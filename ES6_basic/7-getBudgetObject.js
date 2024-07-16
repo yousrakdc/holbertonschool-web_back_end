152fe9c449f6aaa2c806edc36ccb1980928f5158
yousrakdc <yousrakdc@gmail.com>
2024-07-16 14:29:59 +0200

    syntax

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,8 @@
 /* eslint-disable */
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
+    return {
       income,
       gdp,
       capita,
     };
-  
-    return budget;
   }
