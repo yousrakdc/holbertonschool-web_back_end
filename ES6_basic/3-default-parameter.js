ff556f10475b4ba25e58ddddba1bc4c2c74bf056
yousrakdc <yousrakdc@gmail.com>
2024-07-16 11:15:30 +0200

    testing

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-    return initialNumber + (expansion1989 || 89) + (expansion2019 || 19);
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    return initialNumber + expansion1989 + expansion2019;
   }
-  
