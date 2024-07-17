840f0e6f8b1ca10e1c9721336e76b92f4860c9d8
yousrakdc <yousrakdc@gmail.com>
2024-07-17 12:30:22 +0200

    task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,24 +1,19 @@
 /* eslint-disable */
 export default class Building {
     constructor(sqft) {
-        if (new.target === Building) {
-            throw new TypeError('Abstract class "Building" cannot be instantiated directly');
-        }
-
-        if (typeof sqft !== 'number') {
-            throw new TypeError('Sqft must be a number');
-        }
-
-        this._sqft = sqft;
-    }
-
-    // Getter for sqft
-    get sqft() {
-        return this._sqft;
-    }
-
-    // Abstract method that must be implemented by subclasses
-    evacuationWarningMessage() {
+      this._sqft = sqft;
+  
+      if (this.constructor !== Building && this.evacuationWarningMessage
+        === Building.prototype.evacuationWarningMessage) {
         throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+
+    set sqft(newSqft) {
+      this._sqft = newSqft;
     }
 }
