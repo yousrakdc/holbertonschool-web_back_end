85aaeb3ab54426be4e4337e3d08ba1decb540804
yousrakdc <yousrakdc@gmail.com>
2024-07-17 11:00:42 +0200

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        // Type verification
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        if (typeof length !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+
+        // Attribute assignment
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    // Getter and setter for name
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (typeof newName !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = newName;
+    }
+
+    // Getter and setter for length
+    get length() {
+        return this._length;
+    }
+
+    set length(newLength) {
+        if (typeof newLength !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = newLength;
+    }
+
+    // Getter and setter for students
+    get students() {
+        return this._students;
+    }
+
+    set students(newStudents) {
+        if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = newStudents;
+    }
+}
