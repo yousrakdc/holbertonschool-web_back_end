aa8405bb5360204fa3ab117694ca820b9c8c98e4
yousrakdc <yousrakdc@gmail.com>
2024-07-17 16:59:02 +0200

    corrected

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
 export default class Car {
     constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
     }
-  
+
     cloneCar() {
-      return new Car(this._brand, this._motor, this._color);
+        // Create a new instance using the class of the current instance
+        return new this.constructor(this._brand, this._motor, this._color);
     }
-  }
+}
