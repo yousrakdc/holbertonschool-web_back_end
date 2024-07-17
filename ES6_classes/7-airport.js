34cbdb00e706ebb03b78fea8e63b0f8caeaa0ea9
yousrakdc <yousrakdc@gmail.com>
2024-07-17 16:32:49 +0200

    formatting

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -5,27 +5,27 @@ export default class Airport {
         this._code = code;
         }
 
-        get name() {
-        return this._name;
-        }
-
-        get code() {
-        return this._code;
-        }
-
-        set name(name) {
-        this._name = name;
-        }
-        
-        set code(code) {
-        this._code = code;
-        }
-
-        get [Symbol.toStringTag]() {
-        return this._code;
-        }
-
-        toString() {
-        return `[object ${this._code}]`;
-        }
+    get name() {
+    return this._name;
     }
+
+    get code() {
+    return this._code;
+    }
+
+    set name(name) {
+    this._name = name;
+    }
+        
+    set code(code) {
+    this._code = code;
+    }
+
+    get [Symbol.toStringTag]() {
+    return this._code;
+    }
+
+    toString() {
+    return `[object ${this._code}]`;
+    }
+}
