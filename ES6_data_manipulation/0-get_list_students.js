787c4a1e86ad6608db434334cd7328bed85c2646
yousrakdc <yousrakdc@gmail.com>
2024-07-22 13:08:01 +0200

    get students

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-function getListStudents() {
+export default function getListStudents() {
     return [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
