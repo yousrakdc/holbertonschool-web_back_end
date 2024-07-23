0d02c22b1a8f98b1e43498c0793456151ac2ebe7
yousrakdc <yousrakdc@gmail.com>
2024-07-23 09:24:01 +0200

    map

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+
+    return students.map(student => student.id);
+}
