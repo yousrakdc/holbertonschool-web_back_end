5ea9b21af429af92eb508c6d6b7a9791c9a00645
yousrakdc <yousrakdc@gmail.com>
2024-07-24 10:02:17 +0200

    throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+    const queue = [];
+
+    try {
+        const result = mathFunction();
+        queue.push(result);
+    } catch (error) {
+        queue.push(error.message);
+    } finally {
+        queue.push('Guardrail was processed');
+    }
+
+    return queue;
+}
