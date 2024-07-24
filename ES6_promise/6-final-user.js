41d27cb956941d50a24a073f7ee3bd34a3122723
yousrakdc <yousrakdc@gmail.com>
2024-07-24 09:49:51 +0200

    multiple  promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,20 +3,15 @@ import { signUpUser } from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-        .then((results) => {
-            return results.map((result) => {
-                if (result.status === 'fulfilled') {
-                    return {
-                        status: 'fulfilled',
-                        value: result.value
-                    };
-                } else {
-                    return {
-                        status: 'rejected',
-                        value: result.reason
-                    };
-                }
-            });
-        });
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      }
+
+      return { status: 'rejected', value: result.reason.toString() };
+    }));
 }
