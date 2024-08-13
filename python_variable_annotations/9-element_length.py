0d712724540fb6b47496c59c4b162ce675024d56
yousrakdc <yousrakdc@gmail.com>
2024-08-13 10:30:07 +0200

    made a mistake

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,9 +5,9 @@ contains a string from the input list and its length
 """
 
 
-from typing import List, Tuple
+from typing import Iterable, Tuple, List, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """return list of tuples"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return appropriate types"""
     return [(i, len(i)) for i in lst]
