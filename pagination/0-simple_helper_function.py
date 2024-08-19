4c5706f5a97222a8906c4247e7ed26858a26f9ce
yousrakdc <yousrakdc@gmail.com>
2024-08-19 13:25:23 +0200

    base

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 function named index_range that takes two integer arguments page & page_size
 """
 
+
 def index_range(page, page_size):
     """A tuple (start_index, end_index)"""
     start_index = (page - 1) * page_size
