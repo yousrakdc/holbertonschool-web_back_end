39b120c86aa3b96c67682e8b27dc9e0ed6054903
yousrakdc <yousrakdc@gmail.com>
2024-08-19 11:51:37 +0200

    base

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer arguments page & page_size
+"""
+
+def index_range(page, page_size):
+    """A tuple (start_index, end_index)"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
