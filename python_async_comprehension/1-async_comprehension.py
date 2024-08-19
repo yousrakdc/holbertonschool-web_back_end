3b240d0aa388babd5870570ef6087c01a7074478
yousrakdc <yousrakdc@gmail.com>
2024-08-19 10:00:33 +0200

    async comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+collect 10 random numbers using an async comprehensing over async_generator
+return the 10 random numbers.
+'''
+
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    return [number async for number in async_generator()]
