6c2bbd4ab7b4cae2c9e1887facf46e50a5f62388
yousrakdc <yousrakdc@gmail.com>
2024-08-21 11:27:29 +0200

    new id

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """inserts a new document in a collection based on kwargs"""
+from pymongo import InsertOne
 
 
 def insert_school(mongo_collection, **kwargs):
     """Returns the new _id"""
     docs = {}
-    for k, v in kwargs.items():
-        docs[k] = v
+    for i, j in kwargs.items():
+        docs[i] = j
     result = mongo_collection.insert_one(docs)
     return result.new_id
