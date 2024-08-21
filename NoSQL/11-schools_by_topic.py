738259392b4119483a1e61b83ad2b1dc449f918d
yousrakdc <yousrakdc@gmail.com>
2024-08-21 14:22:38 +0200

    specific topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """returns the list of school having a specific topic"""
-from pymongo import MongoClient
-
 
 def schools_by_topic(mongo_collection, topic):
+    """returns list of school with specific topic"""
     return list(mongo_collection.find({"topics": topic}))
