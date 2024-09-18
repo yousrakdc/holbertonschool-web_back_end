1b09fe3f0cb6b030c4d283cf9bdeff768758db36
yousrakdc <yousrakdc@gmail.com>
2024-09-18 10:58:31 +0200

    http

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const url = require('url');
 const countStudents = require('./3-read_file_async');
-const path = process.argv[2];
-
+const databasePath = process.argv[2];
 const app = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
 
@@ -10,20 +9,22 @@ const app = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.statusCode = 200;
     res.end('Hello Holberton School!');
-  } else if (parsedUrl.pathname === '/students') {
+  } 
+
+  else if (parsedUrl.pathname === '/students') {
     res.setHeader('Content-Type', 'text/plain');
     res.statusCode = 200;
-
     res.write('This is the list of our students\n');
 
-    countStudents(path)
+    countStudents(databasePath)
       .then((output) => {
         res.end(output);
       })
       .catch((err) => {
         res.end(err.message);
       });
-  } else {
+  } 
+  else {
     res.statusCode = 404;
     res.end('Not Found');
   }
