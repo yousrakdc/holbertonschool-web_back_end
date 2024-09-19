c84126697cfab252098b1f21c4bf9c92ceaa5730
yousrakdc <yousrakdc@gmail.com>
2024-09-19 10:39:42 +0200

    express fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,31 +1,26 @@
 const express = require('express');
+
 const countStudents = require('./3-read_file_async');
+const database = process.argv[2];
 
 const app = express();
-const port = 1245;
 
 app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Hello Holberton School!');
+  res.type('text').send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  const databasePath = process.argv[2];
-
-  if (!databasePath) {
-    res.status(400).send('Database path is required as a command-line argument');
-    return;
-  }
+  res.type('text');
 
   try {
-    const output = await countStudents(databasePath);
-    res.type('text/plain');
+    const output = await countStudents(database);
     res.send(`This is the list of our students\n${output}`);
-  } catch (error) {
-    res.status(500).send(error.message);
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
+const port = 1245;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
