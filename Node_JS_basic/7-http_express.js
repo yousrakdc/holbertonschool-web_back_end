aebea832850e243e20bb9f15190dd4a10461eb90
yousrakdc <yousrakdc@gmail.com>
2024-09-19 10:47:05 +0200

    eslint fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
 
+const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = express();
