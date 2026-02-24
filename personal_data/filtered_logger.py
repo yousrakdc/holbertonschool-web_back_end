c4405f6e49ab829e7b5506dd13f0931bffa363aa
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-24 14:14:30 +0100

    line too long

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     pattern = f'({"|".join(fields)})=([^{separator}]*)'
     return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}', message)
