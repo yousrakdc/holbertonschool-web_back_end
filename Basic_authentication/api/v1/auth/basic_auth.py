906d7c329fef29293d92a70437d7f332761a1dd6
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:22:50 +0100

    fixes

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,14 +15,14 @@ from models.user import User
 class BasicAuth(Auth):
     """ BasicAuth class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str) -> str:
         """ Extract base64 authorization header
         """
-        if authorization_header is None or type(authorization_header) != str:
+        if authorization_header is None or type(authorization_header) is not str:
             return None
 
         if not authorization_header.startswith("Basic "):
             return None
 
         return authorization_header[6:]
-
