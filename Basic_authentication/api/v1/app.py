5a7019554e9e3f9e89f27c812c1958188e5975e7
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 09:51:43 +0100

    fixed mistake

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -18,8 +18,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 def not_found(error) -> str:
     """ Not found handler
     """
-    return jsonify({"error": "Not found"}), 404, ({"error": "Unauthorized"}), 401
+    return jsonify({"error": "Not found"}), 404, 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401,
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,8 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'],
+                 strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
