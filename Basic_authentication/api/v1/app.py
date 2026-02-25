22ae7f19905baf978afef812f7eb618962c54e2d
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:08:39 +0100

    fixing

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,6 +13,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+auth_type = getenv("AUTH_TYPE")
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -42,15 +50,21 @@ if __name__ == "__main__":
 
 
 @app.before_request
-def before_request_func():
-    """ Before request function
+def before_request_handler():
+    """ Before request handler
     """
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    if auth is None:
+        return
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
