af3e1554a314ce4f6689644ec591b26d9c43c114
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:32:54 +0100

    oups

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,7 +58,7 @@ class BasicAuth(Auth):
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
 
-def user_object_from_credentials(
+    def user_object_from_credentials(
         self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """ User object from credentials
         """
