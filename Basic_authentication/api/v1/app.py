e01cffbe3a85f9fd061cd9d42c4be9c66759c8bc
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:12:17 +0100

    betty

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -22,6 +22,7 @@ else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -62,7 +63,7 @@ def before_request_handler():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-        
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
