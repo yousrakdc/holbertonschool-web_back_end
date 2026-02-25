0ead6f5914fecff8fd778759801453d546fb512f
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:30:57 +0100

    9. Basic - User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,3 +43,17 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user credentials
+        """
+        if (decoded_base64_authorization_header is None or
+                type(decoded_base64_authorization_header) is not str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
