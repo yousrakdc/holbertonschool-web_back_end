cc5a11229acea8aae8a798fa5b30247bc1084d7d
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:19:36 +0100

    wrong imports

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,10 @@
 Basic authentication module
 """
 
-from typing import List, TypeVar
-from flask import request
+import base64
+from typing import Tuple, TypeVar
+from api.v1.auth.auth import Auth
+from models.user import User
 
 """BasicAuth class"""
 class BasicAuth(Auth):
