7c512289a2be99d2adc86ae2efb004b70db64386
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:16:15 +0100

    basic auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic authentication module
+"""
+from typing import List, TypeVar
+from flask import request
+
+class BasicAuth(Auth):
+    """ BasicAuth class
+    """
