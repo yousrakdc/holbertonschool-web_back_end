7ac0945d85c8344b5065ca670e011df38f556f8f
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 10:04:07 +0100

    request val

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -39,3 +39,18 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
+
+@app.before_request
+def before_request_func():
+    """ Before request function
+    """
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
