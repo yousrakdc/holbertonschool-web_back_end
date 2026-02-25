c08350b93c7f0c158e986508166822855587fce1
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-02-25 09:58:34 +0100

    auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+	"""Auth class for handling authentication methods"""
+	def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+		"""Returns False. path and excluded_paths will be used later."""
+		return False
+
+	def authorization_header(self, request=None) -> str:
+		"""Returns None. request will be the Flask request object."""
+		return None
+
+	def current_user(self, request=None) -> TypeVar('User'):
+		"""Returns None. request will be the Flask request object."""
+		return None
+
