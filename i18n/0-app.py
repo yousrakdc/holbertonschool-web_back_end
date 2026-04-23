364736680763e744071eca0d3ec2d7635954691a
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-04-23 09:50:05 +0200

    oups

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
-"""Basic Flask app with Babel setup"""
+
+""" Module for the index page"""
+
 
 from flask import Flask, render_template
-from flask_babel import Babel
-
-
-class Config:
-    """Configuration class for Flask app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
 
 app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
 
 
-@app.route("/")
+@app.route('/')
+
 def index():
-    """Route for the home page"""
-    return render_template("1-index.html")
+    """ Route for the index page
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
---
+++ i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Basic Flask app with Babel setup"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """Route for the home page"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
