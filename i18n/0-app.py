c21e119aa824b9d9505a5c1b42aaf5ed06a980c7
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-04-23 09:45:02 +0200

    pycodestyle

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 
+
 def index():
     """ Route for the index page
     """
@@ -15,4 +16,3 @@ def index():
 
 if __name__ == "__main__":
     app.run()
-    
